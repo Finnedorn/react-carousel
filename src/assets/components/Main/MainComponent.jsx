f958171d9ab2fdc7397cb52894b5209eed0ee6d5
Finnedorn <daniele.giuntoli@outlook.com>
2024-06-14 15:27:42 +0200

    added carousel + animations and bullets

--- src/assets/components/Main/MainComponent.jsx
+++ src/assets/components/Main/MainComponent.jsx
@@ -1,7 +1,7 @@
 import CarouselComponent from "../Carousel/CarouselComponent";
 import posts from "../../data/posts";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MdArrowBackIos as Arrowleft, MdArrowForwardIos as Arrowright } from "react-icons/md";
 
 
@@ -10,6 +10,28 @@ const MainComponent = () => {
 
   const [currentSlide, setCurrentSlide] = useState(0);
   const totalSlides = posts.length;
+  
+  const [slideClass, setSlideClass] = useState("");
+  
+    // se clicco su left applico la classe con l'animazione e imposto l'indice di post all'elemento successivo
+  const leftClick = () => {
+    console.log(currentSlide);
+    setSlideClass("left");
+    setCurrentSlide(currentSlide => currentSlide === 0 ? totalSlides - 1 : currentSlide - 1);
+  };
+  
+  // right
+  const rightClick = () => {
+    console.log(currentSlide);
+    setSlideClass("right");
+    setCurrentSlide(currentSlide => currentSlide === totalSlides - 1 ? 0 : currentSlide + 1);
+  };
+  
+  // setto un timer che mi resetta la classe di slides left or right dopo 5 milisecondi
+  useEffect(() => {
+    const timer = setTimeout(() => setSlideClass(''), 500);
+    return () => clearTimeout(timer);
+  }, [slideClass]);
 
   return (
     <main
@@ -31,15 +53,30 @@ const MainComponent = () => {
       </h1>
       {/* carousel */}
       <div className="container d-flex flex-wrap justify-content-center align-items-center">
-        <Arrowleft onClick={() => setCurrentSlide(currentSlide=> currentSlide === 0 ? totalSlides - 1 : currentSlide - 1)} />
-        <CarouselComponent
-          title={posts[currentSlide].title}
-          image={posts[currentSlide].image}
-          content={posts[currentSlide].content}
-          tags={posts[currentSlide].tags}
-        />
-        <Arrowright onClick={() => setCurrentSlide(currentSlide => currentSlide === totalSlides - 1 ? 0 : currentSlide + 1)} />
-          
+        <div className="d-flex flex-wrap justify-content-center align-items-center btn me-5" onClick={() => leftClick()}>
+          <Arrowleft/>
+        </div>
+        <div className={`slide ${slideClass}`}>
+          <CarouselComponent
+            title={posts[currentSlide].title}
+            image={posts[currentSlide].image}
+            content={posts[currentSlide].content}
+            tags={posts[currentSlide].tags}
+          />
+        </div>
+        <div className="d-flex flex-wrap justify-content-center align-items-center btn ms-5" onClick={() => rightClick()}>
+          <Arrowright/>
+        </div>
+      </div>
+      <div className="container d-flex justify-content-center align-items-center">
+        {Array.from({ length: totalSlides }).map((el,index) => (
+          console.log(index),
+          <div 
+          key={index} 
+          className={currentSlide === index ? "bullet selected" : "bullet"} 
+          onClick={() => setCurrentSlide(index)}>
+          </div>
+        ))}
       </div>
     </main>
   );
